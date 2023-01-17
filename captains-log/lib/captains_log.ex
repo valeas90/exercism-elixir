8d8b8a3a64c57da79c134f0d740c1ba70b49983b
Jhon Company <jonvaleas@gmail.com>
2023-01-17 11:43:33 +0100

    captains-log

---
+++ captains-log/lib/captains_log.ex
@@ -0,0 +1,21 @@
+defmodule CaptainsLog do
+  @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
+
+  def random_planet_class() do
+    Enum.random(@planetary_classes)
+  end
+
+  def random_ship_registry_number() do
+    1000..9999
+    |> Enum.random()
+    |> then(fn n -> "NCC-#{n}" end)
+  end
+
+  def random_stardate() do
+    :rand.uniform() * (42000.0 - 41000.0) + 41000.0
+  end
+
+  def format_stardate(stardate) do
+    "~.1f" |> :io_lib.format([stardate]) |> to_string()
+  end
+end
